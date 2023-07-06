32cc46e03d5178299bbd4dc2eea232e363c96f43
Sarah Zeineh <sarahzenneth@gmail.com>
2023-07-07 00:02:48 +0300

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User_Profile
+from .models import User_Profile, ProfileFeedItem
 
 # Register your models here.
 
 admin.site.register(User_Profile)
+admin.site.register(ProfileFeedItem)
